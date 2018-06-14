82b0ac33628eb58ff70300f96dea7bb7971b4bf4
iocast <iocast@me.com>
2018-06-14 13:24:31 +0200

    added config

--- config.js
+++ config.js
@@ -2,14 +2,16 @@ System.config({
   defaultJSExtensions: true,
   transpiler: false,
   paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
+    'github:*': 'jspm_packages/github/*',
+    'aurelia-value-converters/*': 'dist/*',
+    'npm:*': 'jspm_packages/npm/*'
   },
 
   map: {
-    "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0",
-    "npm:aurelia-polyfills@1.0.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.0.0"
-    }
+    'aurelia-polyfills': 'npm:aurelia-polyfills@1.0.0',
+    'npm:aurelia-polyfills@1.0.0': {
+      'aurelia-pal': 'npm:aurelia-pal@1.0.0'
+    },
+    '@iocast/array-utilities': '@iocast/array-utilities@0.0.2'
   }
 });
